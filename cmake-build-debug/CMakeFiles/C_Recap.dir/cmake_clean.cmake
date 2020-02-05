eaeb2298bae59b1cb4459f0fe42518260de1e365
gogoplodeske <plodeske.g.21@gmail.com>
2020-02-05 03:54:28 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/C_Recap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Recap.dir/main.cpp.o"
+  "C_Recap"
+  "C_Recap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_Recap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+
+
+main(){
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
